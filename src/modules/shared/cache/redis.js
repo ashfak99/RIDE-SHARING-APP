36ff95d50b91782f5bf9f7bf0dda260d661a63e2
ashfak99 <aliakbaralam86@gmail.com>
2026-04-16 19:59:06 +0530

    Redis Driver Methods

--- src/modules/shared/cache/redis.js
+++ src/modules/shared/cache/redis.js
@@ -26,10 +26,6 @@ client.on('reconnecting',()=>{
     console.log(`Redis Network Glitch!! Reconnecting....`)
 })
 
-// (async()=>{
-//     await client.connect();
-// })();
-
 const connectRedis = asyncHandler(async()=>{
     try {
         await client.connect();
@@ -40,6 +36,75 @@ const connectRedis = asyncHandler(async()=>{
     }
 })
 
+const setDriverLocation = asyncHandler(async(driverId, lat, lag)=>{
+    try {
+        const redisKey = "drivers:active";
+
+        if (typeof lat !== 'number' || typeof lng !== 'number' || isNaN(lat) || isNaN(lng) || lat < -90 || lat > 90 || lng < -180 || lng > 180) {
+            throw new Error("Invalid Coordinates");
+        }
+
+        await client.geoAdd(redisKey,{
+            longitude : lag,
+            latitude : lat,
+            member : driverId
+        });
+
+        console.log("Location Updated for Driver : "+ driverId);
+        return true;
+    } catch (error) {
+        console.log("System Error: Failed to update location for "+driverId)
+        console.log(error)
+        return false;
+    }
+})
+
+const findNearestDriver = asyncHandler(async (userLat,userLng,radiusKm)=>{
+    try {
+        const redisKey = "drivers:active";
+
+        const nearbyDrivers = await client.geoSearch(
+            redisKey,
+            { longitude : userLng, latitude : userLat },
+            { radius : radiusKm, unit : 'km'},
+            {WITHDIST : true, SORT : 'ASC', COUNT: 10}
+        );
+
+        if(nearbyDrivers === 0){
+            console.log("No drivers available in "+radiusKm+" km radius.");
+            return [];
+        }
+
+        console.log("Found "+nearbyDrivers.length()+" drivers nearby.");
+        return nearbyDrivers;
+    } catch (error) {
+        console.log("System Error: Failed to search nearby drivers.");
+        console.log(error);
+        return [];
+    }
+})
+
+const cacheRideSession = asyncHandler(async (rideId,rideDate)=>{
+    try {
+        const stringifiedData = JSON.stringify(rideDate);
+        const redisKey = "ride:session:"+rideId;
+        const expiryTimeInSeconds = 3600;
+    
+        await client.set(redisKey,stringifiedData,{EX : expiryTimeInSeconds});
+    
+        console.log("Ride session cached successfully with 1-hour TTL. Ride Id : "+rideId);
+    
+        return true
+    } catch (error) {
+        console.log("System Error: Failed to cache ride session.");
+        console.log(error);
+        return false;
+    }
+})
+
 export {
-    connectRedis
+    connectRedis,
+    setDriverLocation,
+    findNearestDriver,
+    cacheRideSession
 }
